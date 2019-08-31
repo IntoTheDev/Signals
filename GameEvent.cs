d29e5d2020f340a6a42fc300706161781ffbc814
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-08-31 18:11:17 +0500

    Add files via upload

---
+++ GameEvent.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using NaughtyAttributes;
+
+[CreateAssetMenu(menuName = "ToolBox/Game Event")]
+public class GameEvent : ScriptableObject
+{
+	private List<GameEventListener> listeners = new List<GameEventListener>();
+	private int gameEventListenersCount = 0;
+
+	[Button("Raise")]
+	public void Raise()
+	{
+		for (int i = 0; i < gameEventListenersCount; i++)
+			listeners[i].Response();
+	}
+
+	public void AddListener(GameEventListener listener)
+	{
+		listeners.Add(listener);
+		gameEventListenersCount++;
+	}
+
+	public void RemoveListener(GameEventListener listener)
+	{
+		listeners.Remove(listener);
+		gameEventListenersCount--;
+	}
+}
---
+++ GameEventListener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+	public GameEvent gameEvent;
+	[SerializeField]
+	private UnityEvent responseToGameEvent;
+
+	private void OnEnable()
+	{
+		gameEvent.AddListener(this);
+	}
+
+	private void OnDisable()
+	{
+		gameEvent.RemoveListener(this);
+	}
+
+	public void Response()
+	{
+		responseToGameEvent.Invoke();
+	}
+}
