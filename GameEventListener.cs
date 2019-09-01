fda65fe9fa8f7e33213246633451cf56618862d0
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-09-01 14:10:21 +0500

    Add files via upload

--- GameEventListener.cs
+++ GameEventListener.cs
@@ -3,18 +3,37 @@ using UnityEngine.Events;
 
 public class GameEventListener : MonoBehaviour
 {
-	public GameEvent gameEvent;
+	[SerializeField]
+	private GameEvent gameEvent;
 	[SerializeField]
 	private UnityEvent responseToGameEvent;
 
+	private bool isGameEventAttached = true;
+
+	private void Awake()
+	{
+		if (gameEvent == null)
+		{
+			Debug.LogWarning("Attach Game Event to " + name);
+			isGameEventAttached = false;
+			enabled = false;
+		}
+		else
+		{
+			isGameEventAttached = true;
+		}
+	}
+
 	private void OnEnable()
 	{
-		gameEvent.AddListener(this);
+		if (isGameEventAttached)
+			gameEvent.AddListener(this);
 	}
 
 	private void OnDisable()
 	{
-		gameEvent.RemoveListener(this);
+		if (isGameEventAttached)
+			gameEvent.RemoveListener(this);
 	}
 
 	public void Response()
