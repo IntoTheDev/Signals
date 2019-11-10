8e93e00f79bb7a4ab718e421b3e75e1b68d0154c
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-11-10 18:50:36 +0500

    Update ObserverEditor.cs

--- Editor/ObserverEditor.cs
+++ Editor/ObserverEditor.cs
@@ -13,12 +13,24 @@ public class ObserverEditor : EditorWindow
 	private int sceneCount = 0;
 	private int assetsCount = 0;
 
+	private bool isDrawing = false;
+
 	private Vector2 scrollPosition = Vector2.zero;
 
 	[MenuItem("Window/ToolBox/Observer")]
 	public static void ShowWindow()
 	{
 		GetWindow<ObserverEditor>("Observer Window");
+	}
+
+	private void OnFocus()
+	{
+		isDrawing = true;
+	}
+
+	private void OnLostFocus()
+	{
+		isDrawing = false;
 	}
 
 	private void OnGUI()
@@ -30,12 +42,18 @@ public class ObserverEditor : EditorWindow
 
 		GUILayout.Space(50f);
 
+		GUILayout.Label("Game Event Listeners in Assets", EditorStyles.boldLabel);
+
 		EditorGUI.BeginDisabledGroup(true);
-		DrawListeners("Game Event Listeners in Assets", assetsCount, assets);
+		DrawListeners(assetsCount, assets);
+		EditorGUI.EndDisabledGroup();
 
 		GUILayout.Space(50f);
 
-		DrawListeners("Game Event Listeners in Scene", sceneCount, scene);
+		GUILayout.Label("Game Event Listeners in Scene", EditorStyles.boldLabel);
+
+		EditorGUI.BeginDisabledGroup(true);
+		DrawListeners(sceneCount, scene);
 		EditorGUI.EndDisabledGroup();
 
 		GUILayout.EndScrollView();
@@ -49,37 +67,43 @@ public class ObserverEditor : EditorWindow
 
 	private void FindListeners()
 	{
-		if (GUILayout.Button("Find Listeners"))
+		if (gameEvent == null)
 		{
-			GameEventListener[] listeners = Resources.FindObjectsOfTypeAll<GameEventListener>();
+			scene.Clear();
+			assets.Clear();
 
-			scene = new List<GameEventListener>();
-			assets = new List<GameEventListener>();
+			sceneCount = 0;
+			assetsCount = 0;
 
-			int listenersCount = listeners.Length;
-
-			for (int i = 0; i < listenersCount; i++)
-			{
-				GameEventListener listener = listeners[i];
-
-				if (listener.GameEvent == gameEvent)
-				{
-					if (listener.gameObject.scene.IsValid())
-						scene.Add(listener);
-					else
-						assets.Add(listener);
-				}
-			}
-
-			sceneCount = scene.Count;
-			assetsCount = assets.Count;
+			return;
 		}
+
+		GameEventListener[] listeners = Resources.FindObjectsOfTypeAll<GameEventListener>();
+
+		scene.Clear();
+		assets.Clear();
+
+		int listenersCount = listeners.Length;
+
+		for (int i = 0; i < listenersCount; i++)
+		{
+			GameEventListener listener = listeners[i];
+
+			if (listener.GameEvent == gameEvent)
+			{
+				if (listener.gameObject.scene.IsValid())
+					scene.Add(listener);
+				else
+					assets.Add(listener);
+			}
+		}
+
+		sceneCount = scene.Count;
+		assetsCount = assets.Count;
 	}
 
-	private void DrawListeners(string label, int count, List<GameEventListener> collection)
+	private void DrawListeners(int count, List<GameEventListener> collection)
 	{
-		GUILayout.Label(label, EditorStyles.boldLabel);
-
 		for (int i = 0; i < count; i++)
 			collection[i] = EditorGUILayout.ObjectField("", collection[i], typeof(GameEventListener), true) as GameEventListener;
 	}
