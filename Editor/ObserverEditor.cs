3bf5fd78200e4704ea990b316b9fdfbc0dea6416
Vladislav Kinyashov <53948684+IntoTheDev@users.noreply.github.com>
2019-11-11 20:12:35 +0500

    Update

--- Editor/ObserverEditor.cs
+++ Editor/ObserverEditor.cs
@@ -9,9 +9,13 @@ public class ObserverEditor : EditorWindow
 
 	private List<GameEventListener> scene = null;
 	private List<GameEventListener> assets = null;
+	private List<GameEventRaising> raiseScene = null;
+	private List<GameEventRaising> raiseAssets = null;
 
 	private int sceneCount = 0;
 	private int assetsCount = 0;
+	private int raiseSceneCount = 0;
+	private int raiseAssetsCount = 0;
 
 	private Vector2 scrollPosition = Vector2.zero;
 
@@ -27,6 +31,20 @@ public class ObserverEditor : EditorWindow
 
 		MainDraw();
 		FindListeners();
+
+		GUILayout.Label("Game Event Raisers in Assets", EditorStyles.boldLabel);
+
+		EditorGUI.BeginDisabledGroup(true);
+		DrawRaisers(raiseAssetsCount, raiseAssets);
+		EditorGUI.EndDisabledGroup();
+
+		GUILayout.Space(50f);
+
+		GUILayout.Label("Game Event Raisers in Scene", EditorStyles.boldLabel);
+
+		EditorGUI.BeginDisabledGroup(true);
+		DrawRaisers(raiseSceneCount, raiseScene);
+		EditorGUI.EndDisabledGroup();
 
 		GUILayout.Space(50f);
 
@@ -63,18 +81,27 @@ public class ObserverEditor : EditorWindow
 			if (assets != null)
 				assets.Clear();
 
+			if (raiseScene != null)
+				raiseScene.Clear();
+
 			sceneCount = 0;
 			assetsCount = 0;
+			raiseSceneCount = 0;
+			raiseAssetsCount = 0;
 
 			return;
 		}
 
 		GameEventListener[] listeners = Resources.FindObjectsOfTypeAll<GameEventListener>();
+		GameEventRaising[] raisers = Resources.FindObjectsOfTypeAll<GameEventRaising>();
 
 		scene = new List<GameEventListener>();
 		assets = new List<GameEventListener>();
+		raiseScene = new List<GameEventRaising>();
+		raiseAssets = new List<GameEventRaising>();
 
 		int listenersCount = listeners.Length;
+		int raisersCount = raisers.Length;
 
 		for (int i = 0; i < listenersCount; i++)
 		{
@@ -89,13 +116,34 @@ public class ObserverEditor : EditorWindow
 			}
 		}
 
+		for (int i = 0; i < raisersCount; i++)
+		{
+			GameEventRaising raiser = raisers[i];
+
+			if (raiser.GameEvent == gameEvent)
+			{
+				if (raiser.gameObject.scene.IsValid())
+					raiseScene.Add(raiser);
+				else
+					raiseAssets.Add(raiser);
+			}
+		}
+
 		sceneCount = scene.Count;
 		assetsCount = assets.Count;
+		raiseSceneCount = raiseScene.Count;
+		raiseAssetsCount = raiseAssets.Count;
 	}
 
 	private void DrawListeners(int count, List<GameEventListener> collection)
 	{
 		for (int i = 0; i < count; i++)
 			collection[i] = EditorGUILayout.ObjectField("", collection[i], typeof(GameEventListener), true) as GameEventListener;
+	}
+
+	private void DrawRaisers(int count, List<GameEventRaising> collection)
+	{
+		for (int i = 0; i < count; i++)
+			collection[i] = EditorGUILayout.ObjectField("", collection[i], typeof(GameEventRaising), true) as GameEventRaising;
 	}
 }
