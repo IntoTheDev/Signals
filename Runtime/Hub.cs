d0ddbdc3fb7903d323f30f70d6244437db8abb7b
Vladislav Kinyashov <indraayy322@gmail.com>
2021-02-15 18:48:00 +0500

    Update Hub.cs

--- Runtime/Hub.cs
+++ Runtime/Hub.cs
@@ -6,17 +6,10 @@ namespace ToolBox.Signals
 	public static class Hub
 	{
 		private static Dictionary<int, List<IReceiver>> _signals = new Dictionary<int, List<IReceiver>>();
-		private static Dictionary<string, List<IReceiver>> _readableSignals = new Dictionary<string, List<IReceiver>>();
-
-		public static event Action<string, string> OnSignalDispatched = null;
-		public static IReadOnlyDictionary<string, List<IReceiver>> ReadableSignals => _readableSignals;
 
 		public static void Dispatch<T>(in T value)
 		{
 			int hash = typeof(T).GetHashCode();
-#if UNITY_EDITOR && ODIN_INSPECTOR
-			OnSignalDispatched?.Invoke(typeof(T).Name, value.ToString());
-#endif
 
 			if (!_signals.TryGetValue(hash, out var receivers))
 				return;
@@ -35,18 +28,10 @@ namespace ToolBox.Signals
 			{
 				receivers.Add(receiver);
 
-#if UNITY_EDITOR && ODIN_INSPECTOR
-				_readableSignals.TryGetValue(typeof(T).Name, out var value);
-				value.Add(receiver);
-#endif
-
 				return;
 			}
 
 			_signals.Add(hash, new List<IReceiver> { receiver });
-#if UNITY_EDITOR && ODIN_INSPECTOR
-			_readableSignals.Add(typeof(T).Name, new List<IReceiver> { receiver });
-#endif
 		}
 
 		public static void Remove<T>(IReceiver<T> receiver)
@@ -54,14 +39,7 @@ namespace ToolBox.Signals
 			int hash = typeof(T).GetHashCode();
 
 			if (_signals.TryGetValue(hash, out List<IReceiver> receivers) && receivers.Contains(receiver))
-			{
 				receivers.Remove(receiver);
-
-#if UNITY_EDITOR && ODIN_INSPECTOR
-				_readableSignals.TryGetValue(typeof(T).Name, out var value);
-				value.Remove(receiver);
-#endif
-			}
 		}
 	}
 }
