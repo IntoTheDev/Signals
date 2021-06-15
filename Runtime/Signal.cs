6c8817238f9394aba763440a65855a2767ea40e8
Vladislav Kinyashov <indraayy322@gmail.com>
2021-06-15 14:13:41 +0500

    Upd

--- Runtime/Signal.cs
+++ Runtime/Signal.cs
@@ -5,16 +5,16 @@ namespace ToolBox.Signals
     public static class Signal<S> where S : struct, ISignal
     {
         private static readonly List<IReceiver<S>> _receivers = new List<IReceiver<S>>(8);
+        private static S _last = default;
+        private static bool _isSignalWasDispatched = false;
 
-        public static S Last { get; private set; } = default;
-
-        public static void AddListener(IReceiver<S> receiver)
+        public static void AddReceiver(IReceiver<S> receiver)
         {
             if (!_receivers.Contains(receiver))
                 _receivers.Add(receiver);
         }
 
-        public static void RemoveListener(IReceiver<S> receiver)
+        public static void RemoveReceiver(IReceiver<S> receiver)
         {
             if (_receivers.Contains(receiver))
                 _receivers.Remove(receiver);
@@ -22,11 +22,18 @@ namespace ToolBox.Signals
 
         public static void Dispatch(in S value)
         {
-            Last = value;
+            _last = value;
+            _isSignalWasDispatched = true;
             int count = _receivers.Count - 1;
 
             for (int i = count; i >= 0; i--)
                 _receivers[i].Receive(in value);
+        }
+
+        public static bool TryGetLast(out S last)
+        {
+            last = _last;
+            return _isSignalWasDispatched;
         }
     }
 }
